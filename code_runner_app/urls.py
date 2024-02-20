2b266ef8aa2f50e2fe081af8bbdd32b0b2e46498
RANJAN KUMAR <ranjanjmt15@gmail.com>
2024-02-20 10:43:48 +0530

    Remove tracked files from .gitignore

--- code_runner_app/urls.py
+++ code_runner_app/urls.py
@@ -1,8 +1,9 @@
 # code_runner/urls.py
 from django.urls import path
-from .views import code_list, execute_code
+from .views import code_list, external
 
 urlpatterns = [
     path('', code_list, name='code_list'),
-    path('execute/<int:code_id>/', execute_code, name='execute_code'),
+    # path('execute/<int:code_id>/', execute_code, name='execute_code'),
+    # path('external', views.external, name='external'),
 ]
--- code_runner_app/views.py
+++ code_runner_app/views.py
@@ -2,21 +2,29 @@
 from django.shortcuts import render, get_object_or_404
 from .models import PythonCode, CodeExecution
 from .forms import CodeExecutionForm
-import subprocess
+from subprocess import run,PIPE 
+import sys
+# import request
+
 
 def code_list(request):
     codes = PythonCode.objects.all()
     return render(request, 'code_list.html', {'codes': codes})
 
-def execute_code(request, code_id):
-    python_code = get_object_or_404(PythonCode, pk=code_id)
-    form = CodeExecutionForm(request.POST or None)
-    if form.is_valid():
-        parameters = form.cleaned_data['parameters']
-        # Assuming 'python' is in the system's PATH
-        command = f'python {python_code.code_file.path} {parameters}'
-        output = subprocess.getoutput(command)
-        CodeExecution.objects.create(python_code=python_code, parameters=parameters, output=output)
-        # Handle output location configuration here if needed
+def external(request):
+    inp = request.Post.get('parm ')
+    out = run(sys.executable,'/home/digitalranjan/workspace/python_code_runner/task_list/test.py')
+    return True
 
-    return render(request, 'execute_code.html', {'python_code': python_code, 'form': form})
+# def execute_code(request, code_id):
+    # python_code = get_object_or_404(PythonCode, pk=code_id)
+    # form = CodeExecutionForm(request.POST or None)
+    # if form.is_valid():
+    #     parameters = form.cleaned_data['parameters']
+    #     # Assuming 'python' is in the system's PATH
+    #     command = f'python {python_code.code_file.path} {parameters}'
+    #     output = subprocess.getoutput(command)
+    #     CodeExecution.objects.create(python_code=python_code, parameters=parameters, output=output)
+    #     # Handle output location configuration here if needed
+
+    # return render(request, 'execute_code.html', {'python_code': python_code, 'form': form})
